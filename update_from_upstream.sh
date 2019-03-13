37f0e3019bcbc1818ff19f14141a35c279a073f3
Christoph Oechslein <oechslein@gmx.de>
2019-03-13 12:33:27 +0100

    Bugfix to update_from_upstream.sh

--- update_from_upstream.sh
+++ update_from_upstream.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-git checkout master && git fetch -v --progress "upstream" master && git push -v --progress "origin" master:master && git checkout Ender3Pro_MKSGenL && git merge master && git push -v --progress "origin" Ender3Pro_MKSGenL:Ender3Pro_MKSGenL
+git remote add -f upstream https://github.com/KevinOConnor/klipper.git
+git checkout master && git -v --progress pull && git fetch -v --progress "upstream" master && git push -v --progress "origin" master:master && git checkout Ender3Pro_MKSGenL && git merge master && git push -v --progress "origin" Ender3Pro_MKSGenL:Ender3Pro_MKSGenL
+
