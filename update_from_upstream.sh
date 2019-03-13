fdd61b3327544b5497561bca9cc0303cbf4542df
Christoph Oechslein <oechslein@gmx.de>
2019-03-13 12:29:23 +0100

    Bugfix to update_from_upstream.sh

--- update_from_upstream.sh
+++ update_from_upstream.sh
@@ -1,3 +1,3 @@
-#!/bin.sh
+#!/bin/sh
 
 git checkout master && git fetch -v --progress "upstream" master && git push -v --progress "origin" master:master && git checkout Ender3Pro_MKSGenL && git merge master && git push -v --progress "origin" Ender3Pro_MKSGenL:Ender3Pro_MKSGenL
